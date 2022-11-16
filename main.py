a1fe9d7244efb05ce812c8216116136dfebe1255
nicholl74 <mark.nicholl74@gmail.com>
2022-11-16 16:05:45 +0000

    Initial state

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Just learning some Python"""
+#Ranking the top 5 words in a file
 
 fhand = open("C:\\Users\\Mark\\Desktop\\python_files\\intro.txt")
 
@@ -10,3 +11,16 @@ for line in fhand:
         counts[word] = counts.get(word, 0) + 1
 
 print(f'Word count - {counts}')
+
+lst = list()
+
+for k, v in counts.items():
+    lst.append((v, k))
+
+lst.sort(reverse=True)
+
+i = 1
+for k, v in lst[:5]:
+    print(f"At rank number {i} we have '{v}' with a total of {k} appearances")
+    i += 1
+
