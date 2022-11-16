1bfb3dc441ced60599a8f5c3ae5543be3a10ed99
nicholl74 <mark.nicholl74@gmail.com>
2022-11-16 15:57:55 +0000

    Initial state

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 
 fhand = open("C:\\Users\\Mark\\Desktop\\python_files\\intro.txt")
 
+counts = dict()
+
+for line in fhand:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+print(f'Word count - {counts}')
