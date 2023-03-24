cf62307ded5af40c078e0cbeaff0dfedeb2683a0
nicholl74 <mark.nicholl74@gmail.com>
2023-03-24 14:21:55 +0000

    Initial state

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-"""Just learning some Python"""
-#Ranking the top 5 words in a file
+"""Just learning some Python:)"""
+# Ranking the top 5 words in a file
+from pathlib import Path
 
 fhand = open("C:\\Users\\Mark\\Desktop\\python_files\\intro.txt")
+outf = open(Path.cwd() / Path('NewFolder') / "output.txt", "w")
 
+#print(fhand.readlines())
 counts = dict()
 
-for line in fhand:
+
+for line in fhand.readlines():
     words = line.split()
+    outf.write(str(words))
+    outf.write('\n')
     for word in words:
         counts[word] = counts.get(word, 0) + 1
+
 
 print(f'Word count - {counts}')
 
@@ -23,4 +30,6 @@ i = 1
 for k, v in lst[:5]:
     print(f"At rank number {i} we have '{v}' with a total of {k} appearances")
     i += 1
+
+
 
